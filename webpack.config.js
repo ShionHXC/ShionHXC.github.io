af82aa7c982b5918a0bb318c087bced13c090989
huangxc <shion_hxc@163.com>
2017-06-26 11:37:37 +0800

    hello world

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var config = {
+  entry: path.resolve(__dirname, 'app/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      query: {
+            presets: ['es2015', 'react']
+        }
+    },{
+        test:/\.css$/,
+        loader:'style-loader!css-loader'
+    },{
+        test:/\.scss$/,
+        loader:'style-loader!css-loader!sass-loader'
+    },{
+        test:/\.(png|jpg)$/,
+        loader:'url?limit=25000'
+    }]
+  }
+};
+module.exports = config;
